dcb3fc6d25973d9266d0a8ac7ed655860749e734
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-07-19 20:39:35 +0800

    Intellisense

--- IniEditor/MainWindow.xaml.cs
+++ IniEditor/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.CodeCompletion;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using IniEditor.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,6 +35,18 @@ namespace IniEditor
             Editor.Document.Changed += Document_Changed;
             Editor.MouseMove += (o, e) => Method();
             Editor.KeyUp += (o, e) => Method();
+            Editor.TextArea.TextEntering += TextArea_TextEntering;
+            Editor.TextArea.TextEntered += TextArea_TextEntered; ;
+        }
+
+        private void TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("TextEntered  " + Editor.GetCursorWord());
+            Editor.ShowCompletionWindow(Editor.GetCursorWord());
+        }
+        private void TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            //System.Diagnostics.Debug.WriteLine("TextEntering " +e.Text);
         }
 
         private void Document_Changed(object sender, ICSharpCode.AvalonEdit.Document.DocumentChangeEventArgs e)
