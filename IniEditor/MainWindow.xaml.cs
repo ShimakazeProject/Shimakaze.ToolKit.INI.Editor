278b95aea14aa0b7d1b585e250c9a3ea0297eb3d
舰队的偶像-岛风酱！ <frg2089@foxmail.com>
2020-07-19 18:52:16 +0800

    初次提交

---
+++ IniEditor/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace IniEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReLoadTheme();
+
+            Editor.Text = "[Section]\nKey = Value; Summary";
+            Editor.Document.Changed += Document_Changed;
+            Editor.MouseMove += (o, e) => Method();
+            Editor.KeyUp += (o, e) => Method();
+        }
+
+        private void Document_Changed(object sender, ICSharpCode.AvalonEdit.Document.DocumentChangeEventArgs e)
+        {
+            Method();
+        }
+
+        void ReLoadTheme()
+        {
+            using (XmlTextReader reader = new XmlTextReader("IniRule.xml"))
+                Editor.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+        }
+
+        void Method()
+        {
+            var line = Editor.TextArea.Document.GetLineByOffset(Editor.CaretOffset);
+            tbLine.Text = Editor.TextArea.Document.GetText(line.Offset, line.Length);
+        }
+    }
+}
